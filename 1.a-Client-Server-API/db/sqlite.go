db9c1215ce592ab3dedadc3be273f780a1b79b9c
Leonardo Pinho <leonardopinho2005@gmail.com>
2024-05-29 15:26:13 -0300

    server.go

---
+++ 1.a-Client-Server-API/db/sqlite.go
@@ -0,0 +1,49 @@
+package db
+
+import (
+	"context"
+	"github.com/leonardopinho/GoLang/1.a-Client-Server-API/models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"log"
+	"time"
+)
+
+var db *gorm.DB
+
+func InitDb() error {
+	var err error
+
+	db, err = gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect database")
+	}
+
+	err = db.AutoMigrate(&models.USDBRL{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func SaveUSDBRL(data *models.USDBRL) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+	defer cancel()
+
+	err := db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		result := tx.Create(&data)
+		if result.Error != nil {
+			log.Fatal(result.Error)
+			return result.Error
+		}
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	return nil
+}
--- 1.a-Client-Server-API/server.go
+++ 1.a-Client-Server-API/server.go
@@ -1,41 +1,39 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"github.com/leonardopinho/GoLang/1.a-Client-Server-API/core/middleware"
+	"github.com/leonardopinho/GoLang/1.a-Client-Server-API/db"
+	"github.com/leonardopinho/GoLang/1.a-Client-Server-API/models"
 	"github.com/valyala/fastjson"
 	"io"
 	"log"
 	"net/http"
+	"time"
 )
 
-type USDBRL struct {
-	Code       string `json:"code"`
-	Codein     string `json:"codein"`
-	Name       string `json:"name"`
-	High       string `json:"high"`
-	Low        string `json:"low"`
-	VarBid     string `json:"varBid"`
-	PctChange  string `json:"pctChange"`
-	Bid        string `json:"bid"`
-	Ask        string `json:"ask"`
-	Timestamp  string `json:"timestamp"`
-	CreateDate string `json:"create_date"`
-}
-
-type DollarPrice struct {
+type BidResponse struct {
 	Value string `json:"value"`
 }
 
 func main() {
+	err := db.InitDb()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// server
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /cotacao", getDollarPriceHandle)
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Fatal(http.ListenAndServe(":8080", middleware.RecoveryMiddleware(mux)))
 }
 
-func getDollarPriceHandle(w http.ResponseWriter, request *http.Request) {
+func getDollarPriceHandle(w http.ResponseWriter, _ *http.Request) {
 	price, err := getUSDBRL()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
 	if price == nil {
@@ -43,36 +41,52 @@ func getDollarPriceHandle(w http.ResponseWriter, request *http.Request) {
 		return
 	}
 
+	// save in database
+	err = db.SaveUSDBRL(price)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusRequestTimeout)
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
 	// result
-	err = json.NewEncoder(w).Encode(DollarPrice{Value: price.Bid})
+	err = json.NewEncoder(w).Encode(BidResponse{Value: price.Bid})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-func getUSDBRL() (*USDBRL, error) {
+func getUSDBRL() (*models.USDBRL, error) {
 	url := "https://economia.awesomeapi.com.br/json/last/USD-BRL"
 
-	resp, err := http.Get(url)
+	// TODO: lazy context in first time
+	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return nil, err
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Println(err)
 		return nil, err
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 		return nil, err
 	}
 
-	var current USDBRL
+	var current models.USDBRL
 	err = parseJson(body, "USDBRL", &current)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return nil, err
 	}
 
 	return &current, nil
